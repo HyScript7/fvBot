a73be6dd31674da147b7b673f78c28eebabab5a6
HyScript7 <hyscript7@seznam.cz>
2025-05-01 23:40:37 +0200

    Fix: Naming in postgres migration not reflecting Model setup
    Postgres has case-sensitive table names. SQL now reflects state in model files.

--- src/main/resources/db/migration/postgresql/V1__Initial_Model.postgresql.sql
+++ src/main/resources/db/migration/postgresql/V1__Initial_Model.postgresql.sql
@@ -4,14 +4,14 @@ Migration Script
 */
 
 -- Tables
-CREATE TABLE IF NOT EXISTS "Member" (
+CREATE TABLE IF NOT EXISTS "member" (
     "id" BIGSERIAL PRIMARY KEY,
     "discord_id" BIGINT,
     "username" VARCHAR(255),
-    "current_character_id" BIGINT,
+    "current_character_id" BIGINT
 );
 
-CREATE TABLE IF NOT EXISTS "Character" (
+CREATE TABLE IF NOT EXISTS "character" (
     "id" BIGSERIAL PRIMARY KEY,
     "first_name" VARCHAR(255),
     "last_name" VARCHAR(255),
@@ -23,31 +23,31 @@ CREATE TABLE IF NOT EXISTS "Character" (
     "user_id" BIGINT
 );
 
-CREATE TABLE IF NOT EXISTS "Title" (
+CREATE TABLE IF NOT EXISTS "title" (
     "id" BIGSERIAL PRIMARY KEY,
     "prefix" VARCHAR(255),
     "suffix" VARCHAR(255)
 );
 
 -- N:N Join Tables
-CREATE TABLE IF NOT EXISTS "UserTitleGrant" (
+CREATE TABLE IF NOT EXISTS "usertitlegrant" (
     "user_id" BIGINT,
     "title_id" BIGINT
 );
-CREATE TABLE IF NOT EXISTS "CharacterTitleGrant" (
+CREATE TABLE IF NOT EXISTS "charactertitlegrant" (
     "character_id" BIGINT,
     "title_id" BIGINT
 );
 
 -- FKs
-ALTER TABLE "Member" ADD CONSTRAINT fk_current_character_id FOREIGN KEY ("current_character_id") REFERENCES "Character" ("id");
-ALTER TABLE "Character" ADD CONSTRAINT fk_title_id FOREIGN KEY ("title_id") REFERENCES "Title" ("id");
-ALTER TABLE "Character" ADD CONSTRAINT fk_user_id FOREIGN KEY ("user_id") REFERENCES "Member" ("id");
-ALTER TABLE "UserTitleGrant" ADD CONSTRAINT fk_user_grant_user_id FOREIGN KEY ("user_id") REFERENCES "Member" ("id");
-ALTER TABLE "UserTitleGrant" ADD CONSTRAINT fk_user_grant_title_id FOREIGN KEY ("title_id") REFERENCES "Title" ("id");
-ALTER TABLE "CharacterTitleGrant" ADD CONSTRAINT fk_character_grant_character_id FOREIGN KEY ("character_id") REFERENCES "Character" ("id");
-ALTER TABLE "CharacterTitleGrant" ADD CONSTRAINT fk_character_grant_title_id FOREIGN KEY ("title_id") REFERENCES "Title" ("id");
+ALTER TABLE "member" ADD CONSTRAINT fk_current_character_id FOREIGN KEY ("current_character_id") REFERENCES "character" ("id");
+ALTER TABLE "character" ADD CONSTRAINT fk_title_id FOREIGN KEY ("title_id") REFERENCES "title" ("id");
+ALTER TABLE "character" ADD CONSTRAINT fk_user_id FOREIGN KEY ("user_id") REFERENCES "member" ("id");
+ALTER TABLE "usertitlegrant" ADD CONSTRAINT fk_user_grant_user_id FOREIGN KEY ("user_id") REFERENCES "member" ("id");
+ALTER TABLE "usertitlegrant" ADD CONSTRAINT fk_user_grant_title_id FOREIGN KEY ("title_id") REFERENCES "title" ("id");
+ALTER TABLE "charactertitlegrant" ADD CONSTRAINT fk_character_grant_character_id FOREIGN KEY ("character_id") REFERENCES "character" ("id");
+ALTER TABLE "charactertitlegrant" ADD CONSTRAINT fk_character_grant_title_id FOREIGN KEY ("title_id") REFERENCES "title" ("id");
 
 -- UKs
-ALTER TABLE "Member" ADD CONSTRAINT uk_discord_id UNIQUE ("discord_id");
-ALTER TABLE "Character" ADD CONSTRAINT uk_innate_name UNIQUE ("innate_name");
+ALTER TABLE "member" ADD CONSTRAINT uk_discord_id UNIQUE ("discord_id");
+ALTER TABLE "character" ADD CONSTRAINT uk_innate_name UNIQUE ("innate_name");
